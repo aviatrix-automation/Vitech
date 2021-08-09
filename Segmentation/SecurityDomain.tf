47592355491d7491a88252ab69409020b6c80dd7
pbomma-avx <74566557+pbomma-avx@users.noreply.github.com>
2021-08-09 09:30:23 -0500

    Add files via upload

---
+++ Segmentation/SecurityDomain.tf
@@ -0,0 +1,283 @@
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_1" {
+    domain_name = "mtrs-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_2" {
+    domain_name = "ufcw-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_3" {
+    domain_name = "gers-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_4" {
+    domain_name = "pacl-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_5" {
+    domain_name = "prucore-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_6" {
+    domain_name = "trustmark-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_7" {
+    domain_name = "iam-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_8" {
+    domain_name = "thirtytwobj-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_9" {
+    domain_name = "vitech-dev-ng2-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_10" {
+    domain_name = "sfg-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_11" {
+    domain_name = "cfao-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_12" {
+    domain_name = "sifo-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_13" {
+    domain_name = "brs-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_14" {
+    domain_name = "mcers-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_15" {
+    domain_name = "bers-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_16" {
+    domain_name = "bess-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_17" {
+    domain_name = "shared-svc-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_18" {
+    domain_name = "avx-test-dev-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_19" {
+    domain_name = "ingress-routing-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_20" {
+    domain_name = "business-continuity-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_21" {
+    domain_name = "manu-dev-ca"
+}
+
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_22" {
+    domain_name = "ia-dev-ca"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_23" {
+    domain_name = "tmk-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_24" {
+    domain_name = "wolf-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_25" {
+    domain_name = "thirtytwobj-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_26" {
+    domain_name = "bess-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_27" {
+    domain_name = "Shared-svc-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_28" {
+    domain_name = "vitech-ingress-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_29" {
+    domain_name = "sfg-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_30" {
+    domain_name = "brs-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_31" {
+    domain_name = "gers-nva"
+}
+
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_32" {
+    domain_name = "pacl-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_33" {
+    domain_name = "vitech-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_34" {
+    domain_name = "v3locity-mgmt-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_35" {
+    domain_name = "sifo-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_36" {
+    domain_name = "shared-public-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_37" {
+    domain_name = "bers-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_38" {
+    domain_name = "client-ingress-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_39" {
+    domain_name = "mtrs-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_40" {
+    domain_name = "ufcw-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_41" {
+    domain_name = "vitech-ww-mgmt-nva"
+}
+
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_42" {
+    domain_name = "pru-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_43" {
+    domain_name = "pentest-dss-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_44" {
+    domain_name = "cfao-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_45" {
+    domain_name = "avx-test-mgmt-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_46" {
+    domain_name = "avx-test-client-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_47" {
+    domain_name = "pentest-coreadmin-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_48" {
+    domain_name = "ngss-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_49" {
+    domain_name = "dr-mgmt-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_50" {
+    domain_name = "dr-client-ingress-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_51" {
+    domain_name = "dr-bers-or"
+}
+
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_52" {
+    domain_name = "global-db-poc-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_53" {
+    domain_name = "dr-ngss-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_54" {
+    domain_name = "dr-shared-svcs-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_55" {
+    domain_name = "dr-sfg-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_56" {
+    domain_name = "dr-vitech-ingress-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_57" {
+    domain_name = "dr-vitech-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_58" {
+    domain_name = "dr-pru-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_59" {
+    domain_name = "sifo-dr-or"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_60" {
+    domain_name = "canada-mgmt-ca"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_61" {
+    domain_name = "ca-shared-svc-ca"
+}
+
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_62" {
+    domain_name = "manu-ca"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_63" {
+    domain_name = "ca-vitech-ingress-ca"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_64" {
+    domain_name = "antin-eu"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_65" {
+    domain_name = "icg-eu"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_66" {
+    domain_name = "eu-ngss-eu"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_67" {
+    domain_name = "shared-svcs-eu"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_68" {
+    domain_name = "vitechcorp-eu"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_69" {
+    domain_name = "eu-vitech-ingress-eu"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_70" {
+    domain_name = "corp-shared-svc-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_71" {
+    domain_name = "corp-mgmt-nva"
+}
+
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_72" {
+    domain_name = "jira-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_73" {
+    domain_name = "vitech-log-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_74" {
+    domain_name = "ntma-eu"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_75" {
+    domain_name = "eu-mgmt-vsgh-eu"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_76" {
+    domain_name = "vitech-cr-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_77" {
+    domain_name = "cost-optimizer-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_78" {
+    domain_name = "vitech-cr-vpc-va-sg"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_79" {
+    domain_name = "vitechvdi-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_80" {
+    domain_name = "avaitrix-test-nva"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_81" {
+    domain_name = "vdi-workspace-eu"
+}
+
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_82" {
+    domain_name = "ca-diy"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_83" {
+    domain_name = "eu-diy"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_84" {
+    domain_name = "or-client-diy"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_85" {
+    domain_name = "or-vitech-diy"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_86" {
+    domain_name = "nva-client-diy"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_87" {
+    domain_name = "nva-vitech-diy"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_88" {
+    domain_name = "nva-ny-onprem"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_89" {
+    domain_name = "nva-nj-onprem"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_90" {
+    domain_name = "or-xand-onprem"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_91" {
+    domain_name = "or-nj-onprem"
+}
+resource "aviatrix_segmentation_security_domain" "segmentation_security_domain_92" {
+    domain_name = "iam-nva"
+}
---
+++ Segmentation/provider.tf
@@ -0,0 +1,21 @@
+# Specify Aviatrix as the provider with these parameters:
+# source - path to Aviaatrix Terraform provider in the terraform registry
+# version - release version # of Aviatrix Terraform provider
+terraform {
+  required_providers {
+    aviatrix = {
+      source = "AviatrixSystems/aviatrix"
+      version = "2.19.5"
+
+    }
+  }
+}
+# Specify Aviatrix controller detail inside the provider block:
+# controller_ip - public IP address of the controller
+# username - login user name, default is admin
+# password - password
+provider "aviatrix" {
+    controller_ip = ""
+    username = "admin"
+    password = ""
+}
